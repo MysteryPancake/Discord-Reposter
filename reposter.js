3d1952bcfaa1d8fa7e8fa14583c67c19db6e68f4
aaa123657 <70584896+aaa123657@users.noreply.github.com>
2020-09-02 20:40:33 +0530

    Update reposter.js

--- reposter.js
+++ reposter.js
@@ -6,7 +6,7 @@ const fs = require("fs");
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
-client.login("<SECRET_BOT_TOKEN>").catch(console.error);
+client.login("NzUwMzQ1NDM0Njg5NDM3ODA4.X05LpA.3lOwcDu1SNrNTYuIrb8GObGZ2wk").catch(console.error);
 
 client.on("ready", function() {
 	client.user.setActivity(client.guilds.size + " server" + (client.guilds.size === 1 ? "" : "s"), { type: "WATCHING" }).catch(console.error);
